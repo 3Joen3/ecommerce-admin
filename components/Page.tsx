d5fc9a4ef5ab42808289e82d98bad99b33ddb88a
3Joen3 <joalevall@gmail.com>
2025-04-21 21:22:42 +0200

    Added text color to the page component

--- components/Page.tsx
+++ components/Page.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function Page({ children, title }: Props) {
   return (
-    <main className="p-6 w-5/6 bg-neutral-100">
+    <main className="p-6 w-5/6 bg-neutral-100 text-neutral-800">
       <h1 className="text-xl font-bold">{title}</h1>
       {children}
     </main>
