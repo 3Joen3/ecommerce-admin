1327a0f874c250eb91d7c0f8fd343a60e098f56b
3Joen3 <joalevall@gmail.com>
2025-05-12 00:38:15 +0200

    Section component now takes classname as prop aswell

--- ui/components/Section.tsx
+++ ui/components/Section.tsx
@@ -1,10 +1,13 @@
 interface Props {
+  className?: string;
   children: React.ReactNode;
 }
 
-export default function Section({ children }: Props) {
+export default function Section({ className, children }: Props) {
   return (
-    <div className="bg-white p-6 shadow-md border border-neutral-200">
+    <div
+      className={`bg-white p-6 shadow-md border border-neutral-200 ${className}`}
+    >
       {children}
     </div>
   );
