cca232e3c4aafc1cc392ada4524079e3ba53388e
3Joen3 <joalevall@gmail.com>
2025-05-12 20:38:16 +0200

    Made the page components scrollable

--- ui/components/Page.tsx
+++ ui/components/Page.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function Page({ children, title }: Props) {
   return (
-    <main className="p-5 w-5/6 bg-neutral-100 text-neutral-800 space-y-5">
+    <main className="h-screen p-5 w-5/6 bg-neutral-100 text-neutral-800 space-y-5 overflow-y-auto">
       <h1 className="text-xl font-bold">{title}</h1>
       {children}
     </main>
