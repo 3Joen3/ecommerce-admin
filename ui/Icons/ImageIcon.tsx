14838517f011b229693a441e236e300d7d3d8335
3Joen3 <joalevall@gmail.com>
2025-05-14 02:07:33 +0200

    Added upload icon and changed in ImageIcon

--- ui/Icons/ImageIcon.tsx
+++ ui/Icons/ImageIcon.tsx
@@ -6,7 +6,6 @@ export default function ImageIcon(props: SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth={1.5}
       stroke="currentColor"
       {...props}
     >
