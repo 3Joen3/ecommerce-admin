d823ff745a598b22ffffd456d1ee5b24a5f33383
3Joen3 <joalevall@gmail.com>
2025-05-22 02:50:55 +0200

    Will work on Variants later.

--- ui/forms/product-form/VariantsFormSection.tsx
+++ ui/forms/product-form/VariantsFormSection.tsx
@@ -3,8 +3,7 @@ import Section from "../../components/Section";
 export default function VariantsFormSection() {
   return (
     <Section>
-      <p>No variants</p>
-      <button>Add</button>
+        <p>No variants yet.</p>
     </Section>
   );
 }
