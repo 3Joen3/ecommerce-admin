be2084e4b813af7173418972d54d55b3a0e3db6c
3Joen3 <joalevall@gmail.com>
2025-05-22 02:08:22 +0200

    Hardcoded currency to the PriceField

--- ui/forms/components/PriceField.tsx
+++ ui/forms/components/PriceField.tsx
@@ -4,17 +4,16 @@ import { useFormContext } from "react-hook-form";
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   name: string;
-  currency: string;
 }
 
-export default function PriceField({ label, name, currency, ...rest }: Props) {
+export default function PriceField({ label, name, ...rest }: Props) {
   const { register } = useFormContext();
 
   return (
     <Field label={label} name={name}>
       <div className="relative">
         <span className="absolute right-3 top-1/2 -translate-y-1/2 text-sm pointer-events-none text-neutral-600">
-          {currency}
+          SEK
         </span>
         <input
           type="number"
